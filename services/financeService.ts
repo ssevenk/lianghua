003af26766a8967e8943d1b908c8cc762ef64229
lazyman <ssevenk@163.com>
2026-03-14 18:27:31 +0800

    2年

--- services/financeService.ts
+++ services/financeService.ts
@@ -31,10 +31,11 @@ export const calculateValue = (name: string, stock: StockConfig, price: number):
 
   const calculatePev = (currPrice: number) => {
     const zPe = currPrice / stock.动态收益;
+    const g = 1 + (stock.增速 || 0) / 100
     // 沪深300没有目标价，不做pe价值计算（不用历史估值，这个没什么参考意义，利率时代等都已经变了）
-    if (!normalPe) return 0;
-    // 按照3年后（乘以3年增速）估值大概率回归（70%）回归来算
-    return 70 * (normalPe / (zPe / Math.pow(1 + (stock.增速 || 0) / 100, 3)) - 1)
+    if (!normalPe) return (Math.pow(g, 2) - 1) * 100;
+    // 按照3年后（乘以2年增速）估值大概率回归（70%）回归来算
+    return 70 * (normalPe / (zPe / Math.pow(g, 2)) - 1)
   };
 
   const calculatePbv = (currPrice: number) => {
