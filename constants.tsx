cee2de7421cd11c7e77739fd64d8660b2c639e7d
lazyman <ssevenk@163.com>
2026-03-31 00:13:35 +0800

    忘了调整ppmt和茅台的持仓数量

--- constants.tsx
+++ constants.tsx
@@ -39,11 +39,11 @@ export const RAW_PROPERTIES: Property[] = [
   { code: 'SH600938', tag: '石油', num: 4100, danbao: true },
   { code: 'SH518880', tag: '黄金', num: 27200, name: '黄金etf', danbao: true },
   { code: 'SH600809', tag: '中港', num: 0, name: '汾酒', danbao: true },
-  { code: 'SH600519', tag: '中港', num: 800, name: '茅台', danbao: true },
+  { code: 'SH600519', tag: '中港', num: 700, name: '茅台', danbao: true },
   { tag: '黄金', num: 145, name: '国外黄金', code: 'USIAU', exchange: 'us' },
   { tag: '有色', num: 5400, code: 'SH601899', danbao: true },
   // 2000在咪咪账户
-  { tag: '中港', code: 'HK09992', num: 2800 + 2400 + 2000, name: '泡泡玛特', exchange: 'hk' },
+  { tag: '中港', code: 'HK09992', num: 2800 + 3400 + 2000, name: '泡泡玛特', exchange: 'hk' },
 
   // 咪咪账户里
   { code: 'SH511520', tag: '债券', name: '中债', num: 400 },
