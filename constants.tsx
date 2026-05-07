e59657a4a7be3989854de1c1947383c7f1c3fab1
lazyman <ssevenk@163.com>
2026-05-07 09:38:32 +0800

    还款

--- constants.tsx
+++ constants.tsx
@@ -9,7 +9,7 @@
 import { StockConfig, Property } from './types';
 
 export const ZHE_XIAN = 0.04;
-export const ALL_DEBT = 395611
+export const ALL_DEBT = 358111
 export const MI_RATIO = 0.18849268;
 export const MA_RATIO = 0.16288967;
 
@@ -31,7 +31,7 @@ export const TAG_RATIO_MAP: Record<string, number> = {
 
 export const RAW_PROPERTIES: Property[] = [
   { code: 'HK00700', tag: '中港', num: 0, name: '腾讯', exchange: 'hk' },
-  { tag: '现金', num: 137212 + 168218, name: '人民币' },
+  { tag: '现金', num: 267945, name: '人民币' },
   { code: 'USSPYM', tag: '美股', num: 484, exchange: 'us' },
   { tag: '现金', num: 3473 + 6379 + 3855, name: '港币', exchange: 'hk' },
   { tag: '现金', num: 3.14 + 36.07 + 69.23 * 7.6144, name: '美元', exchange: 'us' },
