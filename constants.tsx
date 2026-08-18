f938764a6b9dc4d1ba50eb35e8e46493be0072ed
ssevenk <ssevenk@163.com>
2026-08-18 10:27:00 +0800

    卖茅台

--- constants.tsx
+++ constants.tsx
@@ -32,7 +32,7 @@ export const TAG_RATIO_MAP: Record<string, number> = {
 
 export const RAW_PROPERTIES: Property[] = [
   { code: 'HK00700', tag: '中港', num: 1200, name: '腾讯', exchange: 'hk' },
-  { tag: '现金', num: 464, name: '人民币' },
+  { tag: '现金', num: 516891, name: '人民币' },
   { code: 'USQQQM', tag: '美股', num: 146, exchange: 'us' },
   { tag: '现金', num: 551, name: '港币', exchange: 'hk' },
   { tag: '现金', num:  70 + 89 * 6, name: '美元', exchange: 'us' },
@@ -40,7 +40,7 @@ export const RAW_PROPERTIES: Property[] = [
   { code: 'SH600900', tag: '债券', name: '长电', num: 12000, danbao: true },
   { code: 'SH600938', tag: '石油', num: 5600, danbao: true },
   { code: 'SH518880', tag: '黄金', num: 32700 + 1900, name: '黄金etf' },
-  { code: 'SH600519', tag: '中港', num: 500, name: '茅台', danbao: true },
+  { code: 'SH600519', tag: '中港', num: 100, name: '茅台', danbao: true },
   { code: 'SZ000568', tag: '中港', num: 8100, name: '老窖', danbao: true },
   { tag: '黄金', num: 145, name: '国外黄金', code: 'USIAU', exchange: 'us' },
   { tag: '有色', num: 5400 + 200, code: 'SH601899', danbao: true },
