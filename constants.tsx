b1e9b17e947958aafc7a2cee1b479b019c791777
ssevenk <ssevenk@163.com>
2026-08-22 10:51:13 +0800

    咪咪转出 50000

--- constants.tsx
+++ constants.tsx
@@ -11,8 +11,8 @@ import { StockConfig, Property } from './types';
 export const ZHE_XIAN = 0.04;
 export const ALL_DEBT = 0;
 export const MAX_DEBT = 0;
-export const MI_RATIO = 0.085985805;
-export const MA_RATIO = 0.151580998;
+export const MI_RATIO = 0.0741427461;
+export const MA_RATIO = 0.1535449935;
 
 // 初始资金常量
 export const MI_INITIAL = 337964;
@@ -32,7 +32,7 @@ export const TAG_RATIO_MAP: Record<string, number> = {
 
 export const RAW_PROPERTIES: Property[] = [
   { code: 'HK00700', tag: '中港', num: 1200, name: '腾讯', exchange: 'hk' },
-  { tag: '现金', num: 516891, name: '人民币' },
+  { tag: '现金', num: 516891 - 50000, name: '人民币' },
   { code: 'USQQQM', tag: '美股', num: 146, exchange: 'us' },
   { tag: '现金', num: 551, name: '港币', exchange: 'hk' },
   { tag: '现金', num:  70 + 89 * 6, name: '美元', exchange: 'us' },
